19c6faa15953f4dee51ebaa6c68a3379edb6279c
Alis0n09 <aliliseth2006@hotmail.com>
2025-12-10 11:01:28 -0500

    Clase 02-12-2025

--- modulo_3/nest/blog-backend/src/main.ts
+++ modulo_3/nest/blog-backend/src/main.ts
@@ -1,10 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as bodyParser from 'body-parser';
+import { GlobalHttpExceptionFilter } from './common/filters/http-exception.filter';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.use(bodyParser.json()); // fuerza parseo JSON
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
+  app.useGlobalFilters(new GlobalHttpExceptionFilter());
+
+  app.useStaticAssets(join(__dirname, '..', 'public'),{prefix: 'public'});
   await app.listen(3000);
 }
 bootstrap();
