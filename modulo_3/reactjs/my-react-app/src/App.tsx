017a219e835b5e39036100abab471aa362327f8a
Alis0n09 <aliliseth2006@hotmail.com>
2025-12-10 21:51:09 -0500

    Clase 10-12-2025

---
+++ modulo_3/reactjs/my-react-app/src/App.tsx
@@ -0,0 +1,20 @@
+import ButtonCounter from "./components/ButtonCounter";
+import HelloWorld from "./components/HelloWorld";
+import TextInput from "./components/TextInput";
+import ToggleMessage from "./components/ToggleMessage";
+import WelcomeUser from "./components/WelcomeUser";
+
+function App() {
+  return (
+    <>
+      <HelloWorld />
+      <WelcomeUser />
+      <ButtonCounter />
+      <TextInput />
+      <ToggleMessage />
+
+    </>
+  );
+}
+
+export default App;
---
+++ modulo_3/reactjs/my-react-app/src/components/WelcomeUser.tsx
@@ -0,0 +1,5 @@
+export default function WelcomeUser() {
+  const name = "Alison";
+  const lastName = "Venegas";
+  return <p>Bienvenida, {name} {lastName}</p>;
+}
